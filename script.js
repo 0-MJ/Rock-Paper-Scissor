f8d939c002e6bdaadc710165abe5e76e70781c6d
Mervyn <102972493+0M-J@users.noreply.github.com>
2022-12-03 04:28:19 +0530

    Total revamp of Script

--- script.js
+++ script.js
@@ -1,57 +1,78 @@
-//storing scores//
-let playerScore = 0;
-let computerScore =0;
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-     //Generating  computer choice//
-     function getComputerChoice(){
-        const choices = ["rock","paper","scissor"];
-        let selection = Math.floor(Math.random()*choices.length);
-        return choices[selection];
-      };
-    //User choice //
-     let playerSelection = window.prompt("What do you choose?");
- 
-    //Computer choice //
-     let computerSelection = getComputerChoice();
-     
-    //Player Round //
-     function playRound(playerSelection,computerSelection) {
-       if(playerSelection===computerSelection) {
-        return("It's a draw!");
-       //player chooses rock//
-       } else if ((playerSelection ==="rock")&&(computerSelection==="scissor")){
-        playerScore++;
-        return("You win! Rock beats scissor");
-       } else if ((playerSelection ==="rock")&&(computerSelection==="paper")){
-        computerScore++;
-        return("You loose! paper beats rock");
-       //player chooses scissor//
-       } else if ((playerSelection === "scissor")&&(computerSelection==="paper")){
-        playerScore++;
-        return("You win! scissor beats paper");
-       }else if ((playerSelection === "scissor")&&(computerSelection==="rock")){
-        computerScore++;
-        return("You loose! rock beat scissor");
-       //player chooses paper//
-       }else if ((playerSelection === "paper")&&(computerSelection==="rock")){
-        playerScore++;
-        return("You win! Paper beat rocks");
-       } else if ((playerSelection === "paper")&&(computerSelection==="scissor")){
-        computerScore++;
-        return("You loose! scissor beats paper");
-       }
-     }
-    console.log(playerSelection);  
-    console.log(computerSelection);
-    console.log(playRound(playerSelection,computerSelection));
-  }
- }
-game();
-if(playerScore>computerScore){
-  console.log("PLAYER WON");
-}else if (computerScore>playerScore)
-  console.log("COMPUTER WON");
- 
- 
+//Game//
+let rock = document.getElementById('rock');
+let paper = document.getElementById('paper');
+let scissor = document.getElementById('scissor');
+let player = document.getElementById('youChose');
+let computer = document.getElementById('computerChose');
+
+let playerSelection = 0;
+let computerSelection = 0;
+
+
+rock.addEventListener('click', function rock(){
+    playerSelection = "Rock"
+    player.innerHTML = playerSelection;
+    computerSelection = getComputerChoice();
+    computer.innerHTML = computerSelection;
+    gameLogic(playerSelection,computerSelection);
+})
+   
+paper.addEventListener('click',function(){
+   playerSelection = "Paper"
+   player.innerHTML = playerSelection;
+   computerSelection = getComputerChoice();
+   computer.innerHTML = computerSelection;
+   gameLogic(playerSelection,computerSelection);
+})
+
+scissor.addEventListener('click',function(){
+     playerSelection = "Scissor"
+     player.innerHTML = playerSelection;
+     computerSelection = getComputerChoice();
+     computer.innerHTML = computerSelection;
+     gameLogic(playerSelection,computerSelection);
+})
+
+
+//Generate random Choice for Computer//
+function getComputerChoice(){
+    const choices = ["Rock","Paper","Scissor"];
+    let selection = Math.floor(Math.random()*choices.length);
+    return choices[selection];
+  };
+ //ScoreBoard//
+ let playerScore = 0;
+ let computerScore = 0;
+
+ let playScoreDisplay = document.getElementById('yourScore');
+ playScoreDisplay.addEventListener('click',function(){
+    playScoreDisplay.innerHTML = playerScore;
+ })
+ let computerScoreDisplay = document.getElementById('computerScore');
+ computerScoreDisplay.addEventListener('click',function(){
+    computerScoreDisplay.innerHTML = computerScore;
+ })
+
+
+  //Game Logic//
+function gameLogic(playerSelection,computerSelection){
+ if(playerSelection===computerSelection){ 
+
+ }    
+//player chooses scissor//
+else if ((playerSelection ==="rock")&&(computerSelection==="scissor")){
+     playerScore ++;} 
+else if ((playerSelection ==="rock")&&(computerSelection==="paper")){
+        computerScore++;}
+//player chooses scissor//
+else if ((playerSelection === "Scissor")&&(computerSelection==="Paper")){
+        playerScore ++;}
+else if ((playerSelection === "scissor")&&(computerSelection==="rock")){
+        computerScore++;}
+//player chooses paper//
+else if ((playerSelection === "paper")&&(computerSelection==="rock")){
+        playerScore ++;} 
+else if ((playerSelection === "paper")&&(computerSelection==="scissor")){
+        computerScore++;}
+}           
